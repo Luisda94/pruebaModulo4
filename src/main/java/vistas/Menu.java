7b389da27167e6894219f9cab9b7e9bba66f35c4
Luisda <luisda915@gmail.com>
2024-11-07 20:51:12 -0300

    add class

---
+++ src/main/java/vistas/Menu.java
@@ -0,0 +1,6 @@
+package vistas;
+
+public class Menu extends PlantillaMenu {
+    private AlumnoServicio alumnoServicio = new AlumnoServicio();
+    private ArchivoServicio archivoServicio = new ArchivoServicio();
+}
