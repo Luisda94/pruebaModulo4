9ae6effd9bfbf29571f1a650d620f75c87fdbd79
Luisda <luisda915@gmail.com>
2024-11-07 20:37:05 -0300

    add Getters and setters

--- src/main/java/modelo/Materia.java
+++ src/main/java/modelo/Materia.java
@@ -1,5 +1,20 @@
 package modelo;
 
-public class Materia {
+import java.util.List;
 
+public class Materia {
+    public MateriaEnum getNombre() {
+		return nombre;
+	}
+	public void setNombre(MateriaEnum nombre) {
+		this.nombre = nombre;
+	}
+	public List<Double> getNotas() {
+		return notas;
+	}
+	public void setNotas(List<Double> notas) {
+		this.notas = notas;
+	}
+	private MateriaEnum nombre;
+    private List<Double> notas;
 }
