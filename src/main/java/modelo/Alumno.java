f6bb42a610114920014e9a6fa86d3dc5cd3add1a
Luisda <luisda915@gmail.com>
2024-11-07 20:23:36 -0300

    add atributos y constructor

--- src/main/java/modelo/Alumno.java
+++ src/main/java/modelo/Alumno.java
@@ -1,5 +1,25 @@
 package modelo;
 
-public class Alumno {
+import java.util.List;
 
+public class Alumno {
+    private String rut;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private List<Materia> materias;
+
+    /**
+	 * @param rut
+	 * @param nombre
+	 * @param apellido
+	 * @param direccion
+	 */
+	public Alumno(String rut, String nombre, String apellido, String direccion) {
+		super();
+		this.rut = rut;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.direccion = direccion;
+	}
 }
